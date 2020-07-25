7493e52946968d897b7db71af8eb358b232676ff
Joshua Yannix <joshua.yannix@topbloc.com>
2020-07-25 00:06:30 -0500

    added am pm, and adjusted hours to show 12

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-
 setInterval(function () {
-  var datevar = new Date()
-  document.getElementById('hour').innerHTML = datevar.getHours() + ':';
-  document.getElementById('minute').innerHTML = datevar.getMinutes() + ':';
-  document.getElementById('second').innerHTML = datevar.getSeconds();
+  var dateVar = new Date()
+  var displayHour = dateVar.getHours()
+  var displayMinute = dateVar.getMinutes()
+  var displaySecond = dateVar.getSeconds()
+
+  if (displayHour === 0) {
+    displayHour = '12'
+  } else if (displayHour < 10) {
+    displayHour = '0' + displayHour
+  }
+  else if (displayHour >= 12) {
+    displayHour = displayHour - 12;
+    document.getElementById('timeOfDay').innerHTML = ' PM'
+  }
+
+  if (displayMinute < 10) {
+    displayMinute = '0' + displayMinute
+  }
+
+  if (displaySecond < 10) {
+    displaySecond = '0' + displaySecond
+  }
+
+  document.getElementById('hour').innerHTML = displayHour + ':';
+  document.getElementById('minute').innerHTML = displayMinute + ':';
+  document.getElementById('second').innerHTML = displaySecond + ''
+
 }, 1000)
